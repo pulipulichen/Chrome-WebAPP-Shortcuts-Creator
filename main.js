0dcceb8fddb03412f464cb207bc89a4df288995b
Pulipuli Chen <pulipuli.chen@gmail.com>
2018-06-04 22:21:55 +0800

    0604 沒事沒事

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 var fs = require('fs')
   , ini = require('ini')
   , ws = require('windows-shortcuts')
-  , URL = require('url-parse');;
+  , URL = require('url-parse')
+  , cheerio = require('cheerio')
+  , request = require('request')
+  , faviconUrl = require('favicon-url')
+  , iconv = require('iconv-lite');
 
 var config = ini.parse(fs.readFileSync('./config.ini', 'utf-8'));
 var chromeLocation = config.global.googleChromeLocation;
@@ -18,14 +22,31 @@ request(url, function (error, response, body)
     if (!error && response.statusCode == 200)
     {
         var $ = cheerio.load(body);
-        var title = $("title").text();
+        var title = $("title").text().trim();
+        //title = iconv.encode(title, 'big5');
         
+        faviconUrl(url, {timeout: 2000, minBufferLength: 400, securedOnly: true}, (favicon, dataBufferLength) => {
+            // Only favicons served over https.
+             console.log(favicon)
+            // console.log(dataBufferLength)
+            
+        })
+        
+        //title = iconv.decode(new Buffer(title), "utf8");
+        //console.log(title);
+        if ('' === title.trim()) {
+            const urlObject = new URL(url);
+            title = urlObject.host;
+        }
+
         var linkPath = "D:/Desktop/" + title + ".lnk";
+        //linkPath = iconv.decode(new Buffer(linkPath), "big5");
+        //console.log(linkPath);
         ws.create(linkPath, {
             target: chromeLocation,
             args: " --ignore-certificate-errors --app=" + url,
             //icon: '%windir%\\system32\\wucltux.dll',
         });
-
+            
     }
 });
--- npm_package_install.bat
+++ npm_package_install.bat
@@ -3,3 +3,6 @@ npm install windows-shortcuts
 npm i ini
 npm i url-parse
 npm i cheerio
+npm i request
+npm i favicon-url
+npm i node-iconv
