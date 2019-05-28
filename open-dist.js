e48eeb567ee30b2dda01ebb5ceb2d73b00945702
Pulipuli Chen <pulipuli.chen@gmail.com>
2019-05-28 10:11:17 +0800

    0528 close #5 路徑的部分要怎麼確認好呢...

--- open-dist.js
+++ open-dist.js
@@ -1,1 +1,9 @@
-console.log('@TODO OK')
+//console.log('@TODO OK')
+const exec = require('child_process').exec
+const path = require('path')
+
+if (process.platform === 'win32') {
+  let distPath = path.join('dist', 'chrome-webapp-shortcuts-creator.exe')
+  console.log(distPath)
+  exec(distPath, () => {})
+}
